56688543f2d2a6ee8c5ca7643f6cbae840c80851
Sam <linkihylian@gmail.com>
2018-08-03 10:46:23 -0400

    Se agrego un cuerpo en layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,12 +11,33 @@
   </head>
   <body background="{{asset('assets/images/fondo_azul1024.png')}}">
     <div class="container ">
-    <!--Empieza el contenido de la pagina-->
-    <main role="main" class="container">
-      @yield('contenido')
-    </main>
-    <!--Acaba el contenido de la pagina-->
+      <div class="row mt-3 mb-3">
+        <div class="col-md-12 offset-lm-1 rounded-top" style="background:url({{asset('assets/images/fondo_blanco.png')}})">
+          <img width="100%" data-holder-rendered="true" class="img-responsive" src="{{asset('assets/images/cabecera3.png')}}" alt="Imagen banner1">
+          @yield('nav')
+          <div class="panel panel-default">
+            <div class="container mb-5 mt-2">
+              <div class="row mb-4">
+                <div class="col-md-4 offset-md-4">
+                  <h2 align="center" style="color:#1960A4;">@yield('titulo')</h2>
 
+                </div>
+              </div>
+
+            <!--Empieza el contenido de la pagina-->
+            <main role="main" class="container">
+              @yield('contenido')
+            </main>
+            <!--Acaba el contenido de la pagina-->
+          </div>
+        </div>
+        <footer class="footer">
+          <img class="rounded-bottom" src="{{asset('assets/images/pie.png')}}" width="100%" data-holder-rendered="true" alt="Imagen banner1">
+        </footer>
+
+      </div>
+    </div>
+  </div>
     <!-- JavaScript -->
     <script src="{{ asset("js/jquery-3.3.1.min.js") }}"></script>
     <script src="{{ asset("js/popper.min.js") }}"></script>
