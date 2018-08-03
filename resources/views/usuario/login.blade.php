b2c7b5e9c307257956b17d94ae7f6c91ba8fefdd
Sam <linkihylian@gmail.com>
2018-08-03 14:45:51 -0400

    Agregar icons y mover imagen en login 2

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -3,14 +3,17 @@
 @section ('title','Login')
 
 @section ('contenido')
+  <div class="container">
+
+
   <div class="row">
     <div class="col-md-4 offset-md-4 rounded" style="background:url(assets/images/fondo_azul1024.png)">
-        <div class="col-md-12 mt-3 mb-3 ">
+        <div class="col-md-8 offset-md-2 mt-3 mb-3 ">
           <img src="{{asset('assets/images/logo_saren.png')}}" width="60%" height="100%" alt="" style="margin-left:20%; margin-right:0px;">
         </div>
 
-        <div>
 
+        
       {{ Form::open(array('url' => 'foo/bar')) }}
       <div class="form-group">
         {{ Form::text('usuario', null, array('placeholder' => 'Usuario', 'class' =>'text-center form-control mb-1', 'maxlength'=>'8', 'required'=> 'required')) }}
@@ -19,7 +22,8 @@
         {{ Form::button('<a><i class="fas fa-lock"></i> Olvidastes tu contraseña?</a>',array('class'=> 'text-white btn btn-warning btn-block login mt-1'))  }}
       </div>
       {{ Form::close() }}
-      </div>
+
     </div>
+  </div>
   </div>
 @endsection
