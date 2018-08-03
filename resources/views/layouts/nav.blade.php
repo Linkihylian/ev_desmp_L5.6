4407892bc7dc44c0fc10fc2fef4ded176ecc04b4
A.Hernandez <a05j96@gmail.com>
2018-08-03 12:51:46 -0400

    estructurar evaluacion

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,19 +6,19 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Evaluación
           </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <div class="dropdown-menu " aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="#">Action</a>
             <a class="dropdown-item" href="#">Another action</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#">Something else here</a>
           </div>
         </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">Aprobar evaluación <span class="sr-only">(current)</span> </a>
+        <li class="nav-item ">
+          <a class="nav-link " href="#">Aprobar evaluación </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Evaluar resagado</a>
@@ -26,7 +26,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Reportes</a>
         </li>
-        <li class="nav-item active">
+        <li class="nav-item ">
           <a class="nav-link" href="#">Normas </a>
         </li>
 
--- resources/views/super_administrador/inicio.blade.php
+++ resources/views/super_administrador/inicio.blade.php
@@ -1,10 +1,27 @@
 @extends('layouts/layout')
 @extends('layouts/nav')
-@section ('title','Login')
+@section('title','Login')
 
-@section ('contenido')
+@section('contenido')
 
 
-texto
+<div class="card">
+  <div class="card-body">
+
+
+    <div id="fullpage">
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+	<div class="section">Some section</div>
+</div>
+
+
+  </div>
+</div>
+
+@endsection
+@section('js')
+  <script src="{{ asset("js/fullPage.js") }}" charset="utf-8"></script>
 
 @endsection
