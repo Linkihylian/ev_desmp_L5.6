fb2cfe3ddd250ef292f9ab4533c5b64920ff519d
A.Hernandez <a05j96@gmail.com>
2018-08-09 15:47:05 -0400

    camios

---
+++ resources/views/super_administrador/auditoria/odi.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts/layout')
+@extends('layouts/nav')
+@section('title','Rango de actuación')
+@section("css")
+
+@endsection
+
+@section('contenido')
+
+
+<div class="card">
+  <div class="card-body">
+    textp
+  </div>
+</div>
+
+
+@endsection
+@section('js')
+<script src="{{ asset("js/menu.js") }}" charset="utf-8"></script>
+
+
+<script type="text/javascript">
+
+
+
+</script>
+@endsection
