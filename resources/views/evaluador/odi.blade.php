0e78c6774ea719b32e69e9cea244478902f2a360
Sam <linkihylian@gmail.com>
2018-08-09 08:57:24 -0400

    Cambio de estructura/diseño de crearodi

--- resources/views/evaluador/odi.blade.php
+++ resources/views/evaluador/odi.blade.php
@@ -1,80 +1,81 @@
 @extends('layouts/layout')
 @extends('layouts/nav')
-@section ('title','Inicio')
-@section('titulo')
+@section('title','Evaluaciones realizadas')
 
-@endsection
+@section('contenido')
 
-@section ('contenido')
-  <div class="card">
-    <div class="card-body">
-      <h4 class="text-center"><strong>Objetivos De Desempeño Individual</strong></h4>
-      <br><br>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="card border border-secondary">
-            <div class="card-header text-white bg-secondary">Dirección - Oficina</div>
-            <div class="card-body">
-              <br>
-              <select class="form-control" name="direccion" onchange="">
 
-              </select>
-              <br>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card border border-secondary">
-           <div class="card-header text-white bg-secondary">Coordinaciones</div>
-           <div class="card-body">
-             <br>
-             <select class="form-control" name="">
+<div class="card">
+  <div class="card-body">
 
-             </select>
-             <br>
-             <div class="radio">
-               <label>
 
-                <input type="radio" name="" value="" onclick="" id="odicoor">
-              </label>
-              <div class="radio">
-                <label>
-                  <input type="radio" name="" value="" onclick="" id="odicargo">
-                </label>
-                <div class="radio" style="display:none">
-                  <label>
-                    <input type="radio" name="" value="" onclick="">
-                  </label>
+    <h5 class="card-title text-center pt-5" >Objetivos de Desempeño Individual</h5>
 
-                </div>
-              </div>
-             </div>
-           </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card card-secundary" id="cargo" style="display:none;">
-            <div class="card-header">Cargos</div>
-            <div class="card-body">
-              <select class="form-control selectized" name="cargos" onchange="" tabindex="" style="display:none;">
-                <option value selected="selected"></option>
-              </select>
-              <div class="selectize-control form-control single">
-                <div class="selectize-input items not-full has-options">
-                  <input type="text" autocomplete="off" tabindex style="width:4px;">
-                </div>
-                <div class="selectize-dropdown single form-control" style="display:none;">
+    <div class="row">
 
-                </div>
-              </div>
-
-            </div>
+      <div class="col-md-6 offset-3  mt-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">1. Dirección | Oficina</h5>
+            <select class="form-control" name="" id="direccion_oficina">
+              <option value="1">1</option>
+            </select>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 
+      <div class="col-md-6 offset-3  mt-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">2. Coordinación</h5>
+            <select class="form-control" name="coordinacion">
+              <option value="1">1</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 offset-3  mt-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">3. Cargos</h5>
+            <select class="form-control" name="cargos">
+              <option value="1">1</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-sm-12 pt-5 text-center">
+        <i class="fas fa-angle-double-down"></i>
+      </div>
+
+
+    </div>
+
+
+
+
+    <div class="card table-responsive">
+      <table class="table table-hover" id="tabla_ev_realizadas">
+        <thead class="bg-secondary text-white">
+          <tr>
+            <th >Objetivo De Desempeño Individual</th>
+            <th >Peso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Total</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+</div>
 
 
 
